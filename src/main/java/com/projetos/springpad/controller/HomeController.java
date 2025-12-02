79002f8f4a2a7fb2acec842ccf78a7c7b2e83d2c
fabiano-githu <chaviersantos70@gmail.com>
2025-12-02 10:49:10 -0300

    ATUALIZAÇÃO 02 12
    Atualização de pads

--- src/main/java/com/projetos/springpad/controller/HomeController.java
+++ src/main/java/com/projetos/springpad/controller/HomeController.java
@@ -8,6 +8,7 @@ package com.projetos.springpad.controller;
 import com.projetos.springpad.dto.PadSummaryDTO;
 import com.projetos.springpad.model.PadsModel;
 import com.projetos.springpad.repository.PadsRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,7 +24,7 @@ public class HomeController {
     private PadsRepository padsRepository;
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, HttpSession session) {
         // Busca resumos com status ON, ordenados por createdAt DESC
         List<PadSummaryDTO> padsSummaries = padsRepository.findSummariesByStatusOrderByCreatedAtDesc(PadsModel.Status.ON);
 
--- src/main/resources/static/js/firebaseConfig.js
+++ src/main/resources/static/js/firebaseConfig.js
@@ -5,11 +5,11 @@
  *  - Clique na engrenagem
  *  - Copie os dados do projeto nas chaves abaixo
  */
-const firebaseConfig = {
-  apiKey: "AIzaSyD2KkOlBLAIhMM8JRjLI3V0t9A07WmyAYo",
-  authDomain: "padcoisas-32486.firebaseapp.com",
-  projectId: "padcoisas-32486",
-  storageBucket: "padcoisas-32486.firebasestorage.app",
-  messagingSenderId: "659362551657",
-  appId: "1:659362551657:web:8bf9f2ef66ef5b858d4ff8"
-};
+ const firebaseConfig = {
+   apiKey: "AIzaSyCDoTxqk0zU3KzjSWzsQtK1QdqNdbJoV2g",
+   authDomain: "springpad-86b08.firebaseapp.com",
+   projectId: "springpad-86b08",
+   storageBucket: "springpad-86b08.firebasestorage.app",
+   messagingSenderId: "858874781702",
+   appId: "1:858874781702:web:fc14dda9b2a22d0331a387"
+ };
